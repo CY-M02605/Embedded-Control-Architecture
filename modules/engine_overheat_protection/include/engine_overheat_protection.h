ab7f8178fefce5c045d8aff9ae87398c737d0357
CY-M02605 <mcy02605@gmail.com>
2026-07-02 17:28:45 +0900

    feat: add torque limit and fan-request logic for engine overheat protection

--- modules/engine_overheat_protection/include/engine_overheat_protection.h
+++ modules/engine_overheat_protection/include/engine_overheat_protection.h
@@ -13,6 +13,8 @@
 
 #include "increment_timer.h"
 
+#include "lookup_table_1d.h"
+
 namespace engine_overheat_protection {
 
 enum class EngineOverheatProtectionState{
@@ -28,6 +30,10 @@ class EngineOverheatProtection: public framework::ModuleInterface {
             float oil_low_threshold;
             float oil_high_threshold;
             utility::IncrementTimer::Config increment_timer_config;
+            utility::LookupTable1D<float>::Point* torque_lookup_table_points;
+            std::size_t torque_lookup_table_size;
+            utility::LookupTable1D<float>::Point* fan_lookup_table_points;
+            std::size_t fan_lookup_table_size;
         };
 
         EngineOverheatProtection(
@@ -41,6 +47,10 @@ class EngineOverheatProtection: public framework::ModuleInterface {
 
         const signals::BoolSignal& IsOverheatProtectedRef() const;
 
+        const signals::BoolSignal& IsOverheatTorqueDecreasingRef() const;
+
+        const signals::BoolSignal& IsOverheatTorqueDecreasingRef() const;
+
     private:
         EngineOverheatProtectionState state_;
 
@@ -48,7 +58,13 @@ class EngineOverheatProtection: public framework::ModuleInterface {
         const signals::FloatSignal& oil_temp_;
         const signals::BoolSignal& is_engine_running_;
         signals::BoolSignal is_overheat_protected_output_;
+        
+        signals::FloatSignal is_overheat_torque_decreasing_output_;
+        signals::FloatSignal is_overheat_fan_output_;
+        
         utility::IncrementTimer increment_timer_;
+        utility::LookupTable1D<float> torque_lookup_table_;
+        utility::LookupTable1D<float> fan_lookup_table_;
 
 };
 }
--- modules/engine_overheat_protection/src/engine_overheat_protection.cc
+++ modules/engine_overheat_protection/src/engine_overheat_protection.cc
@@ -13,104 +13,216 @@ EngineOverheatProtection::EngineOverheatProtection(
     const signals::FloatSignal &oil_temp,
     const signals::BoolSignal &is_engine_running,
     framework::Manager &manager) : config_(config),
-                                   oil_temp_(oil_temp),
-                                   is_engine_running_(is_engine_running),
-                                   is_overheat_protected_output_(false, signals::ValidityStatus::VALID),
-                                   increment_timer_(config.increment_timer_config),
-                                   state_(EngineOverheatProtectionState::IDLE)
-{
-    manager.RegisterModule(*this);
+    oil_temp_(oil_temp),is_engine_running_(is_engine_running),
+    is_overheat_protected_output_(false, signals::ValidityStatus::VALID),
+    torque_limit_output_(0.0f, signals::ValidityStatus::INVALID),
+    fan_request_output_(0.0, signals::ValidityStatus::INVALID),
+    increment_timer_(config.increment_timer_config),
+    torque_lookup_table_(
+        config.torque_lookup_table_points, 
+        config.torque_lookup_table_size
+    ),
+    fan_lookup_table_(
+        config.fan_lookup_table_points, 
+        config.fan_lookup_table_size
+    ),
+    state_(EngineOverheatProtectionState::IDLE) {
+        manager.RegisterModule(*this);
 }
 
 void EngineOverheatProtection::Update()
 {
 
-    bool output_;
-    signals::ValidityStatus validation;
+    bool alarm_output = false;
+    signals::ValidityStatus alarm_validation = signals::ValidityStatus::INVALID;
+
+    float t_d_output = 0.0f;
+    signals::ValidityStatus t_d_validation = signals::ValidityStatus::INVALID;
+
+    float fan_output = 0.0f;
+    signals::ValidityStatus fan_validation = signals::ValidityStatus::INVALID;
 
     switch (state_)
     {
     case EngineOverheatProtectionState::IDLE:
-        if (!is_engine_running_.IsValid() || !oil_temp_.IsValid())
-        {
-            output_ = false;
-            validation = signals::ValidityStatus::INVALID;
+        if (!is_engine_running_.IsValid() || !oil_temp_.IsValid()) {
+
+            alarm_output = false;
+            alarm_validation = signals::ValidityStatus::INVALID;
+
+            t_d_output = 0.0f;
+            t_d_validation = signals::ValidityStatus::INVALID;
+
+            fan_output = 0.0f;
+            fan_validation = signals::ValidityStatus::INVALID;
+
             state_ = EngineOverheatProtectionState::IDLE;
+            increment_timer_.Clear();
+
             break;
         }
 
         if (is_engine_running_.GetValue() && oil_temp_.GetValue() >= config_.oil_high_threshold) {
-            output_ = false;
-            validation = signals::ValidityStatus::VALID;
+
             state_ = EngineOverheatProtectionState::COUNTING;
-            increment_timer_.Clear();
+
         } else {
-            output_ = false;
-            validation = signals::ValidityStatus::VALID;
+
             state_ = EngineOverheatProtectionState::IDLE;
+            
         }
+
+        alarm_output = false;
+        alarm_validation = signals::ValidityStatus::VALID;
+
+        t_d_output = 100.0f;
+        t_d_validation = signals::ValidityStatus::VALID;
+
+        fan_output = fan_lookup_table_.LookupTable(oil_temp_.GetValue());
+        fan_validation = signals::ValidityStatus::VALID;
+
+        increment_timer_.Clear();
+
         break;
 
     case EngineOverheatProtectionState::COUNTING:
-        if (!is_engine_running_.IsValid() || !oil_temp_.IsValid())
-        {
-            output_ = false;
-            validation = signals::ValidityStatus::INVALID;
+        if (!is_engine_running_.IsValid() || !oil_temp_.IsValid()) {
+
+            alarm_output = false;
+            alarm_validation = signals::ValidityStatus::INVALID;
+
+            t_d_output = 0.0f;
+            t_d_validation = signals::ValidityStatus::INVALID;
+
+            fan_output = 0.0f;
+            fan_validation = signals::ValidityStatus::INVALID;
+
             state_ = EngineOverheatProtectionState::IDLE;
+            increment_timer_.Clear();
+
             break;
         }
 
-        if (oil_temp_.GetValue() < config_.oil_low_threshold)
-        {
-            output_ = false;
-            validation = signals::ValidityStatus::VALID;
+        if (!is_engine_running_.GetValue()) {
+
+            alarm_output = false;
+            alarm_validation = signals::ValidityStatus::VALID;
+
+            t_d_output = 0.0f;
+            t_d_validation = signals::ValidityStatus::VALID;
+
+            fan_output = fan_lookup_table_.LookupTable(oil_temp_.GetValue());
+            fan_validation = signals::ValidityStatus::VALID;
+
             state_ = EngineOverheatProtectionState::IDLE;
+            increment_timer_.Clear();
+
+            break;
+        }
+
+        if (oil_temp_.GetValue() <= config_.oil_low_threshold) {
+
+            alarm_output = false;
+            alarm_validation = signals::ValidityStatus::VALID;
+
+            t_d_output = 100.0f;
+            t_d_validation = signals::ValidityStatus::VALID;
+
+            fan_output = fan_lookup_table_.LookupTable(oil_temp_.GetValue());
+            fan_validation = signals::ValidityStatus::VALID;
+
+            state_ = EngineOverheatProtectionState::IDLE;
+            increment_timer_.Clear();
+
             break;
         }
 
         increment_timer_.Update();
 
-        if (increment_timer_.IsTimeUp())
-        {
-            output_ = true;
-            validation = signals::ValidityStatus::VALID;
+        if (increment_timer_.IsTimeUp()) {
+
+            alarm_output = true;
+            alarm_validation = signals::ValidityStatus::VALID;
+
             state_ = EngineOverheatProtectionState::PROTECTED;
             increment_timer_.Clear();
+
+        } else {
+
+            alarm_output = false;
+            alarm_validation = signals::ValidityStatus::VALID;
+
+            state_ = EngineOverheatProtectionState::COUNTING;
+
         }
-        else
-        {
-            output_ = false;
-            validation = signals::ValidityStatus::VALID;
-        }
+
+        t_d_output = torque_lookup_table_.LookupTable(oil_temp_.GetValue());
+        t_d_validation = signals::ValidityStatus::VALID;
+
+        fan_output = fan_lookup_table_.LookupTable(oil_temp_.GetValue());
+        fan_validation = signals::ValidityStatus::VALID;
+
         break;
 
     case EngineOverheatProtectionState::PROTECTED:
-        if (!is_engine_running_.IsValid() || !oil_temp_.IsValid())
-        {
-            output_ = false;
-            validation = signals::ValidityStatus::INVALID;
+        if (!is_engine_running_.IsValid() || !oil_temp_.IsValid()) {
+
+            alarm_output = false;
+            alarm_validation = signals::ValidityStatus::INVALID;
+
+            t_d_output = 0.0f;
+            t_d_validation = signals::ValidityStatus::INVALID;
+
+            fan_output = 0.0f;
+            fan_validation = signals::ValidityStatus::INVALID;
+
             state_ = EngineOverheatProtectionState::IDLE;
+            increment_timer_.Clear();
+
             break;
         }
 
-        if (oil_temp_.GetValue() < config_.oil_low_threshold)
-        {
-            output_ = false;
-            validation = signals::ValidityStatus::VALID;
+        if (oil_temp_.GetValue() <= config_.oil_low_threshold) {
+
+            alarm_output = false;
+            alarm_validation = signals::ValidityStatus::VALID;
+
             state_ = EngineOverheatProtectionState::IDLE;
             increment_timer_.Clear();
+
         } else {
-            output_ = false;
-            validation = signals::ValidityStatus::VALID;
+
+            alarm_output = true;
+            alarm_validation = signals::ValidityStatus::VALID;
+        
         }
+
+        t_d_output = torque_lookup_table_.LookupTable(oil_temp_.GetValue());
+        t_d_validation = signals::ValidityStatus::VALID;
+
+        fan_output = fan_lookup_table_.LookupTable(oil_temp_.GetValue());
+        fan_validation = signals::ValidityStatus::VALID;
+
         break;
 
     }
 
-    is_overheat_protected_output_ = signals::BoolSignal(output_, validation);
+    is_overheat_protected_output_ = signals::BoolSignal(alarm_output, alarm_validation);
+
+    torque_limit_output_ = signals::FloatSignal(t_d_output, t_d_validation);
+
+    fan_request_output_ = signals::FloatSignal(fan_output, fan_validation);
+
 }
 
-const signals::BoolSignal &EngineOverheatProtection::IsOverheatProtectedRef() const
-{
+const signals::BoolSignal& EngineOverheatProtection::IsOverheatProtectedRef() const {
     return is_overheat_protected_output_;
+}
+
+const signals::FloatSignal& EngineOverheatProtection::TorqueLimitRef() const {
+    return torque_limit_output_;
+}
+
+const signals::FloatSignal& EngineOverheatProtection::FanRequestRef() const {
+    return fan_request_output_;
 }
