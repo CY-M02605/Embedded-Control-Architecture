51f8dac238bbacd996f67790f30622b5ba0a33bc
CY-M02605 <mcy02605@gmail.com>
2026-07-24 18:25:43 +0900

    feat: create an engine overheat protection Arduino file (uncompleted)

---
+++ Arduino_project/EngineOverheatProtection/EngineOverheatProtection.ino
@@ -0,0 +1,6 @@
+#include "src/framework/manager.h"
+#include "src/framework/module_interface.h"
+#include "src/signals/signal.h"
+#include "src/utility/increment_timer.h"
+#include "src/utility/lookup_table_1d.h"
+
