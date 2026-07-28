157c7b727e585eef4ec453ebc93f7a3e10e11f40
CY-M02605 <mcy02605@gmail.com>
2026-07-28 18:05:37 +0900

    feat: keep on writing engine overheat protection .ino file

--- Arduino_project/EngineOverheatProtection/EngineOverheatProtection.ino
+++ Arduino_project/EngineOverheatProtection/EngineOverheatProtection.ino
@@ -4,3 +4,32 @@
 #include "src/utility/increment_timer.h"
 #include "src/utility/lookup_table_1d.h"
 
+#include <Arduino.h>
+
+// Hardware configuration
+constexpr uint8_t OIL_TEMP_INPUT_PIN = A0;
+constexpr uint8_t IS_ENGINE_RUNNING_INPUT_PIN = A1; 
+
+constexpr uint8_t IS_OVERHEAT_PROTECTION_OUTPUT_PIN = 8;
+constexpr uint8_t TORQUE_LIMIT_OUTPUT_PIN = 9;
+constexpr uint8_t FAN_REQUEST_OUTPUT_PIN = 10;
+
+// Demo configuration
+
+// Framework object
+
+// Shared input signal
+
+// Module configuration
+
+// Module object
+
+// Non-blocking control timer
+
+void Setup() {
+
+}
+
+void loop() {
+
+}
